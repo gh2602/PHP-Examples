e03278ef2615767fd0307f663e9bcc4effed1669
gh2602 <gh2602@gmail.com>
2018-08-28 02:11:41 +0300

    First Constats

--- ex1.php
+++ ex1.php
@@ -1,20 +1,4 @@
 <?php
-if ($_POST) {
-    echo '<pre>';
-    echo(print_r($_POST, true));
-    echo '</pre>';
-}
-?>
-<form action="" method="post">
-    Name: <input type="text" name="personal[name]"/><br/>
-    Email: <input type="text" name="personal[email]"/><br/>
-    Beer: <br/>
-    <select multiple name="beer[]">
-        <option value="warthog">Warthog</option>
-        <option value="guinness">Guinness</option>
-        <option value="stuttgarter">Stuttgarter Schwabenbräu</option>
-    </select><br/>
-    <input type="image" src="im.bmp" name="sub"/>
-
-    <input type="submit" value="submit me!"/>
-</form>
+define("CONSTANT", "Hello world.");
+echo CONSTANT; // outputs "Hello world."
+echo Constant; // outputs "Constant" and issues a notice.
